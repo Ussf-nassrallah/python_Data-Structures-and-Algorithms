08b99e24e610a8e763af64943ee172c8d5ee2bcb
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-05 18:22:45 +0100

    Understanding and implementing singly-linked-list

---
+++ python/singly-linked-list/challenge.py
@@ -0,0 +1,61 @@
+
+"""
+Write a class Node that defines a node of a singly linked list by:
+
+Private instance attribute: data:
+property def data(self): to retrieve it
+property setter def data(self, value): to set it:
+data must be an integer, otherwise raise a TypeError exception with the message data must be an integer
+Private instance attribute: next_node:
+property def next_node(self): to retrieve it
+property setter def next_node(self, value): to set it:
+next_node can be None or must be a Node, otherwise raise a TypeError exception with the message next_node must be a Node object
+Instantiation with data and next_node: def __init__(self, data, next_node=None):
+And, write a class SinglyLinkedList that defines a singly linked list by:
+
+Private instance attribute: head (no setter or getter)
+Simple instantiation: def __init__(self):
+Should be printable:
+print the entire list in stdout
+one node number by line
+Public instance method: def sorted_insert(self, value): that inserts a new Node into the correct sorted position in the list (increasing order)
+You are not allowed to import any module
+
+"""
+
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = Node()
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
--- python/singly-linked-list/test.py
+++ python/singly-linked-list/test.py
@@ -1,15 +1,31 @@
 from singly_linked_list import *
+from challenge import *
 
-my_list = LinkedList()
+# my_list = LinkedList()
+#
+# my_list.display()
+# n1 = my_list.length()
+# print(n1)
+#
+# my_list.append(3)
+# my_list.append(1)
+# my_list.append(2)
+#
+# my_list.display()
+# n2 = my_list.length()
+# print(n2)
 
-my_list.display()
-n1 = my_list.length()
-print(n1)
 
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-
-my_list.display()
-n2 = my_list.length()
-print(n2)
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
